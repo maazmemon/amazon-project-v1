bdb5bd5dfa9550a975078763ee724850dcfa5e06
maazmemon <mmemon4151@gmail.com>
2021-12-18 20:50:46 +0530

    init

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,24 +1,33 @@
+//checkout.js
 import React from "react";
 import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
 import { useStateValue } from "./StateProvider";
 import Subtotal from "./Subtotal";
 
-
 function Checkout() {
   const [{cart}, dispatch] = useStateValue();
+
   return (
     <div className="checkout">
       <div className="checkout--left">
           <img className="checkout--adv" src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/Jupiter21P1/pay_stripe_desk.png" alt="adv" />
         <div>
           <h3 className="checkout--title">Your Shopping Details</h3>
-          {/* CheckoutProduct */}
-          {/* CheckoutProduct */}
+          
+          {cart.map( item => (
+           <CheckoutProduct 
+            id = {item.id}
+            title = {item.title}
+            image = {item.image}
+            price = {item.price}
+           /> 
+          ))}
         </div>
       </div>
 
       <div className="checkout--right">
-          <Subtotal />
+        <Subtotal />
       </div>
     </div>
   );
--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -2,19 +2,25 @@ import React from 'react';
 import './CheckProduct.css';
 
 function CheckoutProduct({id, title, price, image}) {
+    
+    
+    const removeFromCart = () => {
+        
+    }
+    
     return (
-        <div className="checkoutProduct">
-            <img className='checkoutProductImage' src={image} alt='img' />
+        <div className="checkoutproduct">
+            <img className='checkoutproductimage' src={image} alt='img' />
 
-            <div className='CheckoutProduct--info'>
-                <p className='CheckoutProduct--title'>{title}</p>
-                <p className='CheckoutProduct--price'>{price}
+            <div className='checkoutproduct--info'>
+                <p className='checkoutproduct--title'>{title}</p>
+                <p className='checkoutproduct--price'>
                 <bold>₹</bold>
                 <strong>{price}</strong>
                 </p>
-              
+                <button onClick={removeFromCart}>Remove From Cart</button>
             </div>
-            <button>Remove From Cart</button>
+            
 
         </div>
     )
